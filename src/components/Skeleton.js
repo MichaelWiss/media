ae46887e770ea85076817d0d4833b6c1a3431fff
MichaelWiss <michael.wiss@gmail.com>
2024-01-10 15:55:09 -0600

    html animations -tailwind

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,7 +1,20 @@
 import classNames from 'classnames';
 
 function Skeleton({ times }) {
-
+    const outerClassNames = classNames(
+        'relative',
+        'overflow',
+        'bg-gray-200',
+        'rounded',
+        'mb-2.5'
+    );
+    const innerClassNames = classNames();
+    const boxes = Array(times).fill(0).map((_, i) => {
+        return <div key={i} classNames={outerClassNames}>
+           <div classNames={innerClassNames}/>
+     </div>;
+    });
+    return boxes;
 }
 
 export default Skeleton;
