1ec0b95ffd9a38fa2c857169195d66f6efc856a8
MichaelWiss <michael.wiss@gmail.com>
2024-02-09 18:03:16 -0600

    delete request

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -2,36 +2,34 @@ import { useFetchAlbumsQuery, useAddAlbumMutation } from '../store';
 import Skeleton from './Skeleton';
 import ExpandablePanel from './ExpandablePanel';
 import Button from './Button';
+import AlbumsListItem from './AlbumsListitem';
 
 
 function AlbumsList({ user }) {
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
     
     const [ addAlbum, results ] = useAddAlbumMutation();
-    console.log(results);
+   
     const handleAddAlbum = () => {
         addAlbum(user);
     };
 
     let content;
     if (isLoading) {
-        content = <Skeleton times={3} />
+        content = <Skeleton className="h-10 w-full" times={3} />
     } else if (error) {
         content = <div>Error loading albums.</div>
     } else {
-        content = data.map(album => {
-            const header = <div>{album.title}</div>
-            return <ExpandablePanel key={album.id} header={header}>
-                    List of Photos
-            </ExpandablePanel>
+        content = data.map((album) => {
+            return <AlbumsListItem key={album.id} album={album} />;
         });
 
     }
 
     return <div>
-        <div>
-            Albums for {user.name}
-            <Button onClick={handleAddAlbum}>
+        <div className='m-2 flex flex-row items-center justify-between'>
+            <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+            <Button loading={results.isLoading} onClick={handleAddAlbum}>
                 + Add Album
             </Button>
         </div>
