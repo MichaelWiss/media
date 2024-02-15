988af1d38646997ddeb56e6bf84f9a4ce4390d02
MichaelWiss <michael.wiss@gmail.com>
2024-02-15 17:04:01 -0600

    fetchPhotos

---
+++ src/store/apis/photosApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+
+const photosApi = createApi({
+    reducerPath: 'photos',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:3005',
+    }),
+    endpoints(builder) {
+        return {
+            fetchPhotos: builder.query({
+                query: (album) => {
+                    return {
+                        url: '/photos',
+                        params: {
+                            albumId: album.id
+                        },
+                        method: 'GET',
+                    };
+                }
+            }),
+            addPhoto: builder.mutation({}),
+            removePhoto: builder.mutation({}),
+        };
+    }
+});
